62177db8aab45ab9e0e6258edcd872328f2f423a
Fnadona <felipenadona1@gmail.com>
2023-11-09 19:07:13 +0000

    making FormatterUtil a abstract class

--- src/main/java/com/example/customer/utils/FormatterUtil.java
+++ src/main/java/com/example/customer/utils/FormatterUtil.java
@@ -1,6 +1,6 @@
 package com.example.customer.utils;
 
-public class FormatterUtil {
+public abstract class FormatterUtil {
     public static String formatField(String field){
         return field.trim().replaceAll( "[ ]{2,}", " ").toUpperCase();
     }
